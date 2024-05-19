8f5934a181c73e0929596c786464ba98802c59b3
deustenebrarum <gamesevolutionteam@gmail.com>
2024-05-19 14:47:09 +0300

    users

--- structures.cpp
+++ structures.cpp
@@ -1,59 +1,132 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 
-struct Point {
-    int x;
-    int y;
+struct User {
+    const char* username;
+    const char* password;
+    int registration_year;
 };
 
-struct Rect {
-    Point position;
-    int height;
-    int width;
-};
+User* find(User* users, size_t size, const char* username) {
+    for (size_t i = 0; i < size; i++) {
+        if (strcmp(users[i].username, username) == 0) {
+            return users + i;
+        }
+    }
 
-void Rect_Print(const Rect& rect) {
-    for (int i = 0; i < rect.position.y; i++) {
-        std::cout << '\n';
-    }
-    for (int i = 0; i < rect.height; i++) {
-        for (int j = 0; j < rect.position.x; j++) {
-            std::cout << ' ';
-        }
-        for (int j = 0; j < rect.width; j++) {
-            std::cout << '#';
-        }
-        std::cout << '\n';
-    }
+    return nullptr;
 }
 
-struct Fraction {
-    int numerator;
-    int denominator;
-};
+User* add_user(
+    User* users,
+    size_t size,
+    const char* username,
+    const char* password,
+    int year
+) {
+    User* found_user = find(users, size, username);
 
-Fraction Fraction_Sum(Fraction f1, Fraction f2) {
-    int denominator = f1.denominator * f2.denominator;
-    
-    return {
-        f1.numerator * f2.denominator + f2.numerator * f1.denominator,
-        denominator
+    if (found_user != nullptr) {
+        return nullptr;
+    }
+
+    users[size] = User{
+        username,
+        password,
+        year
     };
+
+    return users + size;
+}
+
+User* authorize(
+    User* users,
+    size_t size,
+    const char* username,
+    const char* password
+) {
+    User* user = find(users, size, username);
+
+    if (user == nullptr) return nullptr;
+
+    if (strcmp(user->password, password) != 0) {
+        return nullptr;
+    }
+
+    return user;
+}
+
+void User_Print(const User& user) {
+    std::cout << user.username << '\n'
+        << user.registration_year << "\n\n";
+}
+
+void User_Print(const User* users, size_t users_count) {
+    for (size_t i = 0; i < users_count; i++)
+    {
+        User_Print(users[i]);
+    }
 }
 
 int main() {
-    Rect_Print(Rect{
-        { 3, 3 },
-        5, 7
-    });
-}
+    size_t users_count = 0;
+    size_t users_capacity = 100;
+    User* users = new User[users_capacity];
 
-double length(
-    const Point p1, 
-    const Point p2
-) {
-    int x_diff = p1.x - p2.x;
-    int y_diff = p1.y - p2.y;
-    
-    return sqrt(x_diff * x_diff + y_diff * y_diff);
+    while (true) {
+        int choice = 0;
+
+        std::cout << "1) to register\n"
+            << "2) to authorize\n"
+            << "3) to sort by year";
+
+        std::cin >> choice;
+
+        if (choice == 1) {
+            char* username = new char[100];
+            char* password = new char[100];
+
+            std::cin.ignore();
+            std::cin.getline(username, 100, '\n');
+ 
+            std::cin.getline(password, 100, '\n');
+
+            int year;
+            std::cin >> year;
+
+            add_user(users, users_count, username, password, year);
+            users_count++;
+        }
+        else if (choice == 2) {
+            char username[100];
+            char password[100];
+
+            std::cin.ignore();
+            std::cin.getline(username, 100);
+            std::cin.clear();
+            std::cin.getline(password, 100);
+
+            User* user = authorize(users, users_count, username, password);
+
+            if (user == nullptr) continue;
+
+            std::cout << "Hello " << user->username << '\n';
+            break;
+        } else {
+            for (size_t i = 0; i < users_count; i++)
+            {
+                for (size_t j = 0; j < users_count - 1; j++)
+                {
+                    if (
+                        users[j].registration_year >
+                        users[j + 1].registration_year
+                    ) {
+                        std::swap(users[j], users[j + 1]);
+                    }
+                }
+            }
+            User_Print(users, users_count);
+        }
+    }
 }
