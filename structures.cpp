c8e0763007e9584a5fe07509c7a5a0f5fd833233
deustenebrarum <gamesevolutionteam@gmail.com>
2024-05-30 16:57:18 +0300

    CARS

--- structures.cpp
+++ structures.cpp
@@ -14,24 +14,46 @@ void Car_Print(const Car& car) {
 	std::cout << "=\n";
 }
 
-int main() {
-	const size_t size = 3;
-	Car cars[size];
+void print_field(const Car* cars, size_t size) {
+	system("cls");
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << i + 1 << '.';
+		Car_Print(cars[i]);
+	}
+}
+
+void update_cars(Car* cars, size_t size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		cars[i].distance += cars[i].speed;
+		cars[i].speed = 1 + rand() % 3;
+	}
+}
+
+void initialize_field(Car* cars, size_t size) {
 	for (size_t i = 0; i < size; i++)
 	{
 		cars[i].distance = 0;
 		cars[i].speed = 1 + rand() % 3;
 	}
+}
 
+void run_game(Car* cars, size_t size) {
 	for (size_t _ = 0; _ < 20; _++) {
-		system("cls");
-		for (size_t i = 0; i < size; i++)
-		{
-			std::cout << i << '.';
-			Car_Print(cars[i]);
-			cars[i].distance += cars[i].speed;
-			cars[i].speed = 1 + rand() % 3;
-		}
+		print_field(cars, size);
+		update_cars(cars, size);
 		Sleep(500);
 	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+
+	const size_t size = 10;
+	Car cars[size];
+	
+	initialize_field(cars, size);
+
+	run_game(cars, size);
 }
